6b1380eab1ec72108380e01e6d67408d59d58e05
vineboneto <vineboneto@gmail.com>
2022-01-31 18:51:44 -0300

    docs: create readme

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import firabase from "@react-native-firebase/app";
 import AppLoading from "expo-app-loading";
 import { StatusBar } from "expo-status-bar";
@@ -26,12 +26,12 @@ export default function App() {
       console.log(firebase.apps);
       if (firebase.apps.length === 0) {
         firabase.initializeApp({
-          appId: "1:821834076383:android:931c4409f5b4f7657ed96a",
-          projectId: "myshopping-f430c",
-          apiKey: "AIzaSyCzd095-Y_icv4GHCvySFXAjk5njJ1qBRA",
-          databaseURL: "https://myshopping-f430c.firebaseio.com",
-          messagingSenderId: "821834076383",
-          storageBucket: "myshopping-f430c.appspot.com",
+          appId: process.env.APP_ID!,
+          projectId: process.env.PROJECT_ID!,
+          apiKey: process.env.API_KEY!,
+          databaseURL: process.env.DATABASE_URL,
+          messagingSenderId: process.env.SENDER_ID,
+          storageBucket: process.env.STORAGE_BUCKET,
         });
       } else {
         setLoading(false);
---
+++ readme.md
@@ -0,0 +1,38 @@
+## Firebase Expo
+
+Estudando funcionalidades de firebase
+
+## Suporte
+
+- [x] Android
+- [ ] iOS
+
+## Variáveis de ambiente
+
+Essas variáveis são necessárias para realizar a conexão com seu projeto no firebase, sua documentação possui as descrição de algumas, as outras você que lute, essa conexão é realizada no arquivo `App.tsx`.
+
+```shell
+APP_ID=
+PROJECT_ID=
+API_KEY=
+DATABASE_URL=
+SENDER_ID=
+STORAGE_BUCKET=
+```
+
+### Requisitos
+
+- Conta no firebase
+- NodeJS 14.x
+- Expo
+
+## Instalação Develop
+
+Configure um projeto no firebase seguindo sua documentação para realizar a instalação
+
+```shell
+# Instalar dependências
+$ yarn
+# Executar em emulador
+$ yarn android
+```
